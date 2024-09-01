b75bd4d73519bbb180f674fbbcc36c001b4326aa
Barsoti <thiagobarsoti@hotmail.com>
2024-09-01 15:45:07 -0300

    Controle de Fluxo

---
+++ Controle de Fluxo/Controle de Fluxo Ternário/Controle de Fluxo Ternário/src/ResultadoEscolar.java
@@ -0,0 +1,11 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+        
+        int nota = 7;
+
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado"; // é uma abreviação do bloco if
+        System.out.println(resultado);
+
+    }
+}
